fce7a934ccb8f385badc4b67fa79435b679d949c
Swarna Pragathi <mandadapuswarna@gmail.com>
2026-09-12 20:28:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 02-shopcart
+
+## Run locally
+
+```bash
+npm install
+npm run dev
+```
+
+## Build
+
+```bash
+npm run build
+```
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <h2>ShopCart</h2>
+        <input
+          placeholder="Search
+										products..."
+        />
+        <b>
+          Cart 
+        </b>
+      </nav>
+    </div>
+  )
+}
+
+export default App;
